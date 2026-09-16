0ff645c5ce639939200694b7ef8bce65c3e46ea7
Mike Williams <Mike.Williams@libertyMutual.com>
2026-09-16 18:43:40 +0000

    Replaced header fallback

--- capstone-client/src/api.ts
+++ capstone-client/src/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosHeaders } from 'axios'
 
 const api = axios.create({
   baseURL: '/api',
@@ -8,8 +8,8 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
 
   if (token) {
-    config.headers = config.headers ?? {}
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers = config.headers ?? new AxiosHeaders()
+    config.headers.set('Authorization', `Bearer ${token}`)
   }
 
   return config
